c565c810213f21d24d19e214dc6f90b7516f0c1e
unknown <onaseer@medcouture.com>
2019-08-30 13:48:08 -0500

    added slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,11 @@ $(document).ready(()=>{
     console.log('I am working');
 
 
+     
+    // //   <!-- Initialize the plugin -->
+    //   $('#exampleSlider').multislider();
+      
+    //   <!-- Initialize with options, if needed -->
+
     // $('#splash').backstretch('assets/images/splash.png')
 })
